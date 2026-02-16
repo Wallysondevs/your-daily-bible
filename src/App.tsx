15d28e8f00026100b39d731918aa18fee445c62d
Wallysondevs <wallyson.emprendedor@gmail.com>
2026-02-16 14:48:12 -0300

    Remove Lovable references and configure GitHub Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Header } from "@/components/Header";
 import Index from "./pages/Index";
 import BooksPage from "./pages/BooksPage";
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <Header />
         <main className="pb-20 md:pb-0">
           <Routes>
@@ -34,7 +34,7 @@ const App = () => (
             <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
